112ca9ee673a2ffbea96cb2e8208b81b3e5967fe
Your Name <you@example.com>
2024-08-07 14:33:58 +0700

    lint

--- src/renderer/utils/createDebouncedWithKey.ts
+++ src/renderer/utils/createDebouncedWithKey.ts
@@ -3,8 +3,8 @@ export const createDebouncedWithKey = (
     func: (key: Key) => Promise<Result>,
     rememberFor: number,
   ) => {
-    let activeTasks = {} as Record<Key, Promise<Result>>;
-    let lastCheckTimes = {} as Record<Key, undefined | number>;
+    const activeTasks = {} as Record<Key, Promise<Result>>;
+    const lastCheckTimes = {} as Record<Key, undefined | number>;
 
     return async (key: Key) => {
       const now = Date.now();
